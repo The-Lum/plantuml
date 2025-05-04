255a58bb4f68de87c2c0dac7ebcefb5ccc615908
The-Lum <86879521+The-Lum@users.noreply.github.com>
2025-05-04 11:33:45 +0200

    🩹 fix import

--- src/main/java/net/sourceforge/plantuml/skin/rose/ComponentRoseNewpage.java
+++ src/main/java/net/sourceforge/plantuml/skin/rose/ComponentRoseNewpage.java
@@ -44,6 +44,7 @@ import net.sourceforge.plantuml.skin.AbstractComponent;
 import net.sourceforge.plantuml.skin.Area;
 import net.sourceforge.plantuml.skin.ArrowConfiguration;
 import net.sourceforge.plantuml.style.ISkinParam;
+import net.sourceforge.plantuml.style.PName;
 import net.sourceforge.plantuml.style.Style;
 
 public class ComponentRoseNewpage extends AbstractComponent {
